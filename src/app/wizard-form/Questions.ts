3633dc7918c17b7a841a7cfe8fc1e14894fd25d0
AFleischer <afleischer1337@gmail.com>
2018-11-04 08:45:37 -0800

    debuggin pt 1

--- src/app/wizard-form/Questions.ts
+++ src/app/wizard-form/Questions.ts
@@ -1,25 +1,25 @@
 export interface Question {
-    formId: string;  //Identifies the properties of the form
-    questionId: string;  //  Need
-    question: string;   //The text of the question
-    type: textarea | text | password | select | radio | date;
-    answerType: string | number | date | dropdown;  //...;
-    answers: answerType[];  //an array of answerTypes
+    formId: string,  //Identifies the properties of the form
+    questionId: string,  //  Need
+    question: string,   //The text of the question
+    type: textarea | text | password | select | radio | date,
+    answerType: string | number | date | dropdown,  //...;
+    answers: answerType[]  //an array of answerTypes
     //The compiler doesn't like this... 
     //Do I have to define it first?  The compiler didn't throw an error earlier...
     //
     controls : {
-        validationControls: email | password| date | locked; //....other validators
-        submitHandler : onSubmit(); //Where do I define this function? 
+        validationControls: email | password| date | locked, //....other validators
+        submitHandler : onSubmit() //Where do I define this function? 
     };
-    questionUpdatedOn: Date;
-    answeredOn: Date;
+    questionUpdatedOn: Date,
+    answeredOn: Date,
 };
 
 
 export class ExampleQuestion {
-    question_name : "What is your Name?";
-    response_type : 'shortText';
+    question_name : "What is your Name?",
+    response_type : 'shortText'
 };
 
 //Need a userID? 
--- src/app/wizard-form/wizard-modal-card/wizard-modal-card.component.ts
+++ src/app/wizard-form/wizard-modal-card/wizard-modal-card.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import { MatStepperModule } from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { mockViews } from '../question_views';
+
 @Component({
   selector: 'app-wizard-modal-card',
   templateUrl: './wizard-modal-card.component.html',
@@ -7,9 +18,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WizardModalCardComponent implements OnInit {
 
-  constructor() { }
+	/*=============
+	Variable declarations
+	=============*/
 
-  ngOnInit() {
-  }
+	question_viewlist = mockViews;
+		stepper_views = question_viewlist.question_views.viewList;
+
+
+	constructor(){
+		console.log("component constructed!");
+	}
+
+	ngOnInit(){
+
+		console.log("component started!");
+
+		console.log("stepper_views recieved is:"+stepper_views);
+	}
+
 
 }
